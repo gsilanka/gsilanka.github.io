dbb4780abad2c42a1bb30107d8a7cf0f46fff274
dineetha <sudam1st@gmail.com>
2020-08-26 00:57:14 +0530

    Added CSS style and background image

--- gsi.js
+++ gsi.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
-    
+    $("#centered").css("top", function() {
+        return ($(document).height()/2 - $("#centered").height()/2);
+    });
+    $("#centered").css("left", function() {
+        return ($(document).width()/2 - $("#centered").width()/2);
+    });
 });
