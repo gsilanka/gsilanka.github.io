e76d79ece86a5b973f40a313a666eb186729f042
dineetha <sudam1st@gmail.com>
2020-08-25 23:28:55 +0530

    Edited js file and removed json file. (ghpages support load from txt file but not post)

--- gsi.js
+++ gsi.js
@@ -1,8 +1,3 @@
 $(document).ready(function() {
-    $.post("gsi.json", "",
-        function (data, textStatus, jqXHR) {
-            $("#d1").text(data.menu.id);
-        },
-        "json"
-    );
+    
 });
